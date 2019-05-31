0929676dc1dc8148b44786c69b1ba0a730f90d5e
Yuhang Miao <miaoyh@bu.edu>
2019-05-31 12:39:17 -0400

    spring initialize

---
+++ src/main/java/com/mmall/concurrecy/ConcurrecyApplication.java
@@ -0,0 +1,13 @@
+package com.mmall.concurrecy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrecyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrecyApplication.class, args);
+	}
+
+}
